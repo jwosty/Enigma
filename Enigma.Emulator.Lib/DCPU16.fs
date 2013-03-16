68062a940618728e35ee3866f15f7ffdf09f189e
J. Wostenberg <awosty@gmail.com>
2013-03-16 15:03:03 -0600

    Added functions to get and set registers

--- Enigma.Emulator.Lib/DCPU16.fs
+++ Enigma.Emulator.Lib/DCPU16.fs
@@ -10,3 +10,25 @@ module Registers =
   let mutable Z = 0
   let mutable I = 0
   let mutable J = 0
+  
+  let get dst =
+    match dst with
+      | "A" -> A
+      | "B" -> B
+      | "C" -> C
+      | "X" -> X
+      | "Y" -> Y
+      | "Z" -> Z
+      | "I" -> I
+      | "J" -> J
+  
+  let set dst src =
+    match dst with
+      | "A" -> A <- src
+      | "B" -> B <- src
+      | "C" -> C <- src
+      | "X" -> X <- src
+      | "Y" -> Y <- src
+      | "Z" -> Z <- src
+      | "I" -> I <- src
+      | "J" -> J <- src
