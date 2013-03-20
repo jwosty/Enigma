2d4ed144c5561350b69c36506e64bc3593eda4dd
J. Wostenberg <awosty@gmail.com>
2013-03-19 20:14:32 -0600

    Decided to bundle the assembler and emulator into one tool; creating master project

---
+++ EnigmaTool/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+module EnigmaTool.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+
+[<assembly: AssemblyTitle("EnigmaTool")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("J. Wostenberg")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
