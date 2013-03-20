e6dc2646b4ae67e95f6c345275dbaf793833c1d3
J. Wostenberg <awosty@gmail.com>
2013-03-19 20:38:40 -0600

    Wrote a module to emulate simpe 16-bit RAM

--- Enigma.Emulator.Lib/DCPU16.fs
+++ Enigma.Emulator.Lib/DCPU16.fs
@@ -1,4 +1,5 @@
 module Enigma.Emulator.Lib.DCPU16
+open System
 open System.Collections.Generic
 
 // Readable/writeable register memory
@@ -16,3 +17,20 @@ module Registers =
   let get reg = if registers.ContainsKey reg then registers.[reg] else failwith "No such register exists"
   
   let set reg value = if registers.ContainsKey reg then registers.[reg] <- value else failwith "No such register exists"
+
+module RAM =
+  let private memory = Array.zeroCreate<uint16> 0xffff // Maximum memory size for the DCPU-16
+  
+  let private failBadMemLoc (loc : int) = failwith <| sprintf "Bad memory location (0x%s)!" (Convert.ToString (loc, 16))
+  
+  let read loc =
+    try
+      memory.[loc]
+    with :? System.IndexOutOfRangeException as e ->
+      failBadMemLoc loc
+  
+  let write loc dat =
+    try
+      memory.[loc] <- dat
+    with :? System.IndexOutOfRangeException as e ->
+      failBadMemLoc loc
