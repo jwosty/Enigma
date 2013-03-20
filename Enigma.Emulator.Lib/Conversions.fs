f4696a689aaca86859a0498068612df37c1e4184
J. Wostenberg <awosty@gmail.com>
2013-03-19 18:12:17 -0600

    Made extractBits a globally-accessible function (not defined locally in split anymore)

--- Enigma.Emulator.Lib/Conversions.fs
+++ Enigma.Emulator.Lib/Conversions.fs
@@ -7,14 +7,14 @@ type System.Convert with
     let x = str.PadLeft (l, '0')
     x |> Seq.map (fun ch -> if ch = '1' then true else false) |> Seq.toList
 
+// Note: when extracting, position 0 = MSB (usually the rightmost bit)
+let extractBits n bStart bEnd =
+  let mask = ~~~(~~~0us <<< (bEnd - bStart + 1))
+  (n >>> bStart) &&& mask
+
 // Split the instruction into its 3 components with the format:
 // (destination : int, source : int, opcode : int)
 let split n =
-  // Note: when extracting, position 0 = MSB (usually the rightmost bit)
-  let extractBits bStart bEnd =
-    let mask = ~~~(~~~0us <<< (bEnd - bStart + 1))
-    (n >>> bStart) &&& mask
-  
-  (extractBits 10 15,   // Destination
-   extractBits 5 9,     // Source
-   extractBits 0 4)     // Opcode
+  (extractBits n 10 15,   // Destination
+   extractBits n 5 9,     // Source
+   extractBits n 0 4)     // Opcode
