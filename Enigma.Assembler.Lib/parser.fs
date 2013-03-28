f6da6d6b25e1da7aaabb0190063a4f48f9bc7c0a
J. Wostenberg <awosty@gmail.com>
2013-03-28 07:19:59 -0600

    Made basicOpcode return an Opcode instead of a basicOpcode so it can be used with specialOpcode later

--- Enigma.Assembler.Lib/parser.fs
+++ Enigma.Assembler.Lib/parser.fs
@@ -23,13 +23,13 @@ let isBasicOpcode x =
     | None -> false
 
 // Try: CharStream.ParseString ("ADD SUB", 0, "ADD SUB".Length, basicOpcode, (), "")
-let basicOpcode : Parser<BasicOpcode,_> =
+let basicOpcode : Parser<Opcode,_> =
   fun stream ->
     let reply = letterGroup stream
     if reply.Status = Ok then
       printfn "Using %A" reply.Result
       match basicOpcodes.TryFind reply.Result with
-      | Some x -> Reply x
+      | Some x -> Reply(Basic x)
       | None -> Reply (Error, messageError <| "No such opcode `" + reply.Result + "'")
     else
       Reply(Error, expected <| "Two-argument opcode")
