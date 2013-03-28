1a9fe233352666ecff0111d1b4f7fb903f2f1738
J. Wostenberg <awosty@gmail.com>
2013-03-28 16:28:25 -0600

    Created an fsi script file (should have done that a while ago)

---
+++ Enigma.Assembler.Lib/script.fsx
@@ -0,0 +1,12 @@
+#r "FParsecCS.dll"
+#r "FParsec.dll"
+#I "/Users/alanwostenberg/Projects/FSharp/Enigma/Enigma.Assembler.Lib/"
+#load "ast.fs"
+#load "parser.fs"
+open System
+open FParsec
+open Enigma.Assembler.Parser
+
+run basicOpcode "SET"
+
+run destinationOperand "C"
