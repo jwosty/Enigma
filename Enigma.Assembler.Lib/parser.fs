c1ab9e77799c0dd2ecc4cef268d775ca5e8f1c09
J. Wostenberg <awosty@gmail.com>
2013-03-28 14:50:07 -0600

    Wrote a parser for destination operands

--- Enigma.Assembler.Lib/parser.fs
+++ Enigma.Assembler.Lib/parser.fs
@@ -17,19 +17,31 @@ let enumNamesValues<'E when 'E : comparison> =
 
 let letterGroup : Parser<string, unit> = many1Satisfy isLetter .>> spaces
 let basicOpcodes = enumNamesValues<BasicOpcode>
+let registers = enumNamesValues<Register>
 let isBasicOpcode x =
   match basicOpcodes.TryFind x with
     | Some _ -> true
     | None -> false
+
+// Note to self: refactor this and basicOpcode -- lots of duplicate code!
+// For now, destination operand can only be a register
+let destinationOperand : Parser<Register,_> =
+  fun stream ->
+    let reply = letterGroup stream
+    if reply.Status = Ok then
+      match registers.TryFind reply.Result with
+      | Some x -> Reply x
+      | None -> Reply (Error, messageError <| "No such register `" + reply.Result + "'")
+    else
+      Reply(Error, expected <| "Destination operand")
 
 // Try: CharStream.ParseString ("ADD SUB", 0, "ADD SUB".Length, basicOpcode, (), "")
 let basicOpcode : Parser<Opcode,_> =
   fun stream ->
     let reply = letterGroup stream
     if reply.Status = Ok then
-      printfn "Using %A" reply.Result
       match basicOpcodes.TryFind reply.Result with
-      | Some x -> Reply(Basic x)
+      | Some x -> Reply (Basic x)
       | None -> Reply (Error, messageError <| "No such opcode `" + reply.Result + "'")
     else
       Reply(Error, expected <| "Two-argument opcode")
