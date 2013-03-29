3ee6817e8c29e3a61588e235b4b67c8097ee46fe
J. Wostenberg <awosty@gmail.com>
2013-03-29 14:21:57 -0600

    Extracted a bunch of code into Engima.Assembler.Basics

--- Enigma.Assembler.Lib/parser.fs
+++ Enigma.Assembler.Lib/parser.fs
@@ -2,56 +2,10 @@ module Enigma.Assembler.Parser
 open System
 open FParsec
 open Ast
+open Enigma.Assembler.Basics
 
-let enumNamesValues<'E when 'E : comparison> =
-  let values = Enum.GetValues typeof<'E>
-  let result =
-    List.init
-      (values.GetLength 0)
-      (fun i ->
-        let v = values.GetValue i
-        (v |> string, unbox<'E> v))
-    |> Map.ofList
-  result
-
-let basicOpcodes = enumNamesValues<BasicOpcode>
-let specialOpcodes = enumNamesValues<SpecialOpcode>
-let registers = enumNamesValues<Register>
-// Parses whitespaces other newlines
-let ws = manyChars (pchar ' ' <|> pchar '\t')
-let letterGroup : Parser<string, unit> = many1Satisfy isLetter .>> ws
 // A comma that can be surrounded by whitespaces
 let argSep : Parser<string, unit> = ws >>. pstring "," .>> ws
-// Like Map.tryFind, but searches case-insensitively (only operates on string maps!)
-let tryFindCI key (map : Map<string, _>) =
-  let result = ref None
-  map |> Map.iter (fun k v -> if String.Equals (key, k, System.StringComparison.CurrentCultureIgnoreCase) then result := Some v)
-  !result
-
-// Executes the given parser and applies the convertsion to the reult (on success)
-let (>!) (parser : Parser<'TResult,_>) conversion =
-  fun stream ->
-    let reply = parser stream
-    if reply.Status = Ok then
-      conversion reply.Result
-    else
-      Reply(Error, reply.Error)
-
-let letterGroupSearchParser (stuff : Map<_,_>) onMatch onMismatch onError : Parser<_,_> =
-  fun stream ->
-    let reply = letterGroup stream
-    if reply.Status = Ok then
-      match stuff |> tryFindCI reply.Result with
-        | Some x -> onMatch x
-        | None -> onMismatch reply
-    else
-      onError ()
-let simpleLetterGroupSearchParser stuff mismatchMessage errorMessage =
-  letterGroupSearchParser
-    stuff
-    (fun x -> Reply x)
-    (fun reply -> Reply (Error, messageError <| mismatchMessage + " `" + reply.Result + "'"))
-    (fun () -> Reply(Error, errorMessage))
 
 let literal : Parser<_, unit> = puint16 >! (fun v -> Reply (Lit v))
 let register = simpleLetterGroupSearchParser registers "Invalid register" (expected "register")
