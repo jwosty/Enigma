ee24a20160db1e19890da2e0ea03662bebd2cc6f
J. Wostenberg <awosty@gmail.com>
2013-09-13 15:34:46 -0600

    Removed Xamarin-Studio generated comment

--- Enigma.Assembler.Lib/Tokenizer.fs
+++ Enigma.Assembler.Lib/Tokenizer.fs
@@ -1,8 +1,3 @@
-
-// NOTE: If warnings appear, you may need to retarget this project to .NET 4.0. Show the Solution
-// Pad, right-click on the project node, choose 'Options --> Build --> General' and change the target
-// framework to .NET 4.0 or .NET 4.5.
-
 module Enigma.Assembler.Lib.Tokenizer
 open System.Text.RegularExpressions
 open Microsoft.FSharp.Reflection
