fea6c8d291bd4ad3c4d32785e58bcb73b2af040e
J. Wostenberg <awosty@gmail.com>
2013-09-13 13:23:21 -0600

    Wrote an Opcode union in the AST

--- Enigma.Assembler.Lib/AbstractSyntaxTree.fs
+++ Enigma.Assembler.Lib/AbstractSyntaxTree.fs
@@ -13,3 +13,32 @@ type Value =
   | Literal of int16
   // Your traditional pointer, which appears in brackets in DCPU-16 assembly, e.g. [0x2A] vs 0x2A 
   | Pointer of Value
+
+type Opcode =
+  // -----------------
+  // - Basic Opcodes -
+  // -----------------
+  | SET
+  // Math
+  | ADD | SUB
+  | MUL | MLI | DIV | DVI
+  | MOD | MDI
+  // Bitwise logic
+  | AND | BOR | XOR
+  // Shifting
+  | SHR | ASR | SHL
+  // Branching
+  | IFB | IFC | IFE | IFN
+  | IFG | IFA | IFL | IFU
+  // Other stuff
+  | ADX | SBX
+  | STI | STD
+  
+  // -------------------
+  // - Special Opcodes -
+  // -------------------
+  | JSR
+  // Interrupt stuff
+  | INT | IAG | IAS | IAQ
+  // Hardware devices
+  | HWN | HWQ | HWI
