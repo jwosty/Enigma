fc7a6f5879e05e818ffede732b0c114a1cf362e9
J. Wostenberg <awosty@gmail.com>
2013-09-13 17:39:49 -0600

    Completed nameRegister map and renamed opcodeNameCaseMap to nameOpcodeMap

--- Enigma.Assembler.Lib/Parser.fs
+++ Enigma.Assembler.Lib/Parser.fs
@@ -5,13 +5,16 @@ open Microsoft.FSharp.Reflection
 open AbstractSyntaxTree
 open Tokenizer
 
-let opcodeNameCaseMap = FSharpType.GetUnionCases(typeof<Opcode>) |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||])) |> Map.ofArray
-let registerNameCaseMap =
+let nameOpcodeMap = FSharpType.GetUnionCases(typeof<Opcode>) |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||])) |> Map.ofArray
+let nameRegisterMap =
   let names = ["RegA"; "RegB"; "RegC"; "RegX"; "RegY"; "RegZ"; "RegI"; "RegJ"; "SP"; "PC"; "EX"]
-  FSharpType.GetUnionCases(typeof<Value>) |> Array.filter (fun u -> List.exists ((=) u.Name) names)
+  FSharpType.GetUnionCases(typeof<Value>)
+  |> Array.filter (fun u -> List.exists ((=) u.Name) names)
+  |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||]))
+  |> Map.ofArray
 
 let tokenToOpcode tok : Opcode option =
   try
-    Some(unbox opcodeNameCaseMap.[unionCaseName tok])
+    Some(unbox nameOpcodeMap.[unionCaseName tok])
   with
     :? System.Collections.Generic.KeyNotFoundException -> None
