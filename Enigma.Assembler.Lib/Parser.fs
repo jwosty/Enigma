2c809bf3e3914a614e41af44d8e477c851590320
J. Wostenberg <awosty@gmail.com>
2013-09-13 17:04:28 -0600

    added stuff

--- Enigma.Assembler.Lib/Parser.fs
+++ Enigma.Assembler.Lib/Parser.fs
@@ -1,3 +1,14 @@
 module Enigma.Assembler.Lib.Parser
+open Enigma.Assembler.Lib
+open GeneralFunctions
+open Microsoft.FSharp.Reflection
 open AbstractSyntaxTree
 open Tokenizer
+
+let opcodeNameCaseMap = FSharpType.GetUnionCases(typeof<Opcode>) |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||])) |> Map.ofArray
+
+let tokenToOpcode tok =
+  try
+    Some(opcodeNameCaseMap.[unionCaseName tok])
+  with
+    :? System.Collections.Generic.KeyNotFoundException -> None
--- Enigma.Assembler.Lib/script.fsx
+++ Enigma.Assembler.Lib/script.fsx
@@ -1,8 +1,13 @@
 #I "/Users/alanwostenberg/Projects/FSharp/Enigma/Enigma.Assembler.Lib/"
 #load "GeneralFunctions.fs"
 #load "Tokenizer.fs"
+#load "AbstractSyntaxTree.fs"
+#load "Parser.fs"
 open Enigma.Assembler.Lib
 open Tokenizer
+open GeneralFunctions
+open AbstractSyntaxTree
+open Parser
 
 tokenize None [] "SET A, B"
 tokenize None [] "set a, b"
