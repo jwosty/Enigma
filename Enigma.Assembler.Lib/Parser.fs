1dee62cb70db6f81ac0b78646a0a2b8ba3affebd
J. Wostenberg <awosty@gmail.com>
2013-09-13 17:30:06 -0600

    Bound register name-case map

--- Enigma.Assembler.Lib/Parser.fs
+++ Enigma.Assembler.Lib/Parser.fs
@@ -6,6 +6,9 @@ open AbstractSyntaxTree
 open Tokenizer
 
 let opcodeNameCaseMap = FSharpType.GetUnionCases(typeof<Opcode>) |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||])) |> Map.ofArray
+let registerNameCaseMap =
+  let names = ["RegA"; "RegB"; "RegC"; "RegX"; "RegY"; "RegZ"; "RegI"; "RegJ"; "SP"; "PC"; "EX"]
+  FSharpType.GetUnionCases(typeof<Value>) |> Array.filter (fun u -> List.exists ((=) u.Name) names)
 
 let tokenToOpcode tok : Opcode option =
   try
