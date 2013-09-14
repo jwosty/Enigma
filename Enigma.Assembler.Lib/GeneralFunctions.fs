519027ffbab073bc74ed6e46ddad50ce36d22e76
J. Wostenberg <awosty@gmail.com>
2013-09-14 11:03:27 -0600

    Rearranged stuff

--- Enigma.Assembler.Lib/GeneralFunctions.fs
+++ Enigma.Assembler.Lib/GeneralFunctions.fs
@@ -1,34 +1,7 @@
 module Enigma.Assembler.Lib.GeneralFunctions
 open Microsoft.FSharp.Reflection
-open Enigma.Assembler.Lib.Tokenizing
-open Enigma.Assembler.Lib.Parsing
 
 // Adds item to list if item satisfies predicate
 let addIf list predicate item = if predicate(item) then list @ [item] else list
 
 let unionCaseName case = (fst <| FSharpValue.GetUnionFields(case, case.GetType())).Name
-
-
-let tokenToValue = function
-  | Token.RegA -> Some AbstractSyntaxTree.RegA | Token.RegB -> Some AbstractSyntaxTree.RegB | Token.RegC -> Some AbstractSyntaxTree.RegC
-  | Token.RegX -> Some AbstractSyntaxTree.RegX | Token.RegY -> Some AbstractSyntaxTree.RegX | Token.RegZ -> Some AbstractSyntaxTree.RegZ
-  | Token.RegI -> Some AbstractSyntaxTree.RegI | Token.RegJ -> Some AbstractSyntaxTree.RegJ | Token.SP -> Some AbstractSyntaxTree.SP
-  | Token.PC -> Some AbstractSyntaxTree.PC | Token.EX -> Some AbstractSyntaxTree.EX
-  | _ -> None
-
-let tokenToBasicOpcode = function
-  | Token.SET -> Some AbstractSyntaxTree.SET
-  | Token.ADD -> Some AbstractSyntaxTree.ADD | Token.SUB -> Some AbstractSyntaxTree.SUB
-  | Token.MUL -> Some AbstractSyntaxTree.MUL | Token.MLI -> Some AbstractSyntaxTree.MLI | Token.DIV -> Some AbstractSyntaxTree.DIV | Token.DVI -> Some AbstractSyntaxTree.DVI
-  | Token.AND -> Some AbstractSyntaxTree.AND | Token.BOR -> Some AbstractSyntaxTree.BOR | Token.XOR -> Some AbstractSyntaxTree.XOR
-  | Token.SHR -> Some AbstractSyntaxTree.SHR | Token.ASR -> Some AbstractSyntaxTree.ASR | Token.SHL -> Some AbstractSyntaxTree.SHL
-  | Token.IFB -> Some AbstractSyntaxTree.IFB | Token.IFC -> Some AbstractSyntaxTree.IFC | Token.IFE -> Some AbstractSyntaxTree.IFE | Token.IFN -> Some AbstractSyntaxTree.IFE
-  | Token.IFG -> Some AbstractSyntaxTree.IFG | Token.IFA -> Some AbstractSyntaxTree.IFA | Token.IFL -> Some AbstractSyntaxTree.IFL | Token.IFU -> Some AbstractSyntaxTree.IFU
-  | Token.ADX -> Some AbstractSyntaxTree.ADX | Token.SBX -> Some AbstractSyntaxTree.SBX | Token.STI -> Some AbstractSyntaxTree.STI | Token.STD -> Some AbstractSyntaxTree.STD
-  | _ -> None
-
-let tokenToSpecialOpcode = function
-  | Token.JSR -> Some AbstractSyntaxTree.JSR
-  | Token.INT -> Some AbstractSyntaxTree.INT | Token.IAG -> Some AbstractSyntaxTree.IAG | Token.IAS -> Some AbstractSyntaxTree.IAS | Token.IAQ -> Some AbstractSyntaxTree.IAQ
-  | Token.HWN -> Some AbstractSyntaxTree.HWN | Token.HWQ -> Some AbstractSyntaxTree.HWQ | Token.HWI -> Some AbstractSyntaxTree.HWI
-  | _ -> None
--- Enigma.Assembler.Lib/Parser.fs
+++ Enigma.Assembler.Lib/Parser.fs
@@ -1,18 +1,41 @@
 module Enigma.Assembler.Lib.Parsing.Parser
-open Enigma.Assembler.Lib
-open GeneralFunctions
 open Microsoft.FSharp.Reflection
+open Enigma.Assembler.Lib.GeneralFunctions
 open Enigma.Assembler.Lib.Tokenizing
-open Enigma.Assembler.Lib.Parsing.AbstractSyntaxTree
+open Enigma.Assembler.Lib.Parsing
 
-let nameBasicOpcodeMap = FSharpType.GetUnionCases(typeof<BasicOpcode>) |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||])) |> Map.ofArray
-let nameSpecialOpcodeMap = FSharpType.GetUnionCases(typeof<SpecialOpcode>) |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||])) |> Map.ofArray
+let nameBasicOpcodeMap = FSharpType.GetUnionCases(typeof<AbstractSyntaxTree.BasicOpcode>) |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||])) |> Map.ofArray
+let nameSpecialOpcodeMap = FSharpType.GetUnionCases(typeof<AbstractSyntaxTree.SpecialOpcode>) |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||])) |> Map.ofArray
 let nameRegisterMap =
   let names = ["RegA"; "RegB"; "RegC"; "RegX"; "RegY"; "RegZ"; "RegI"; "RegJ"; "SP"; "PC"; "EX"]
-  FSharpType.GetUnionCases(typeof<Value>)
+  FSharpType.GetUnionCases(typeof<AbstractSyntaxTree.Value>)
   |> Array.filter (fun u -> List.exists ((=) u.Name) names)
   |> Array.map (fun caseInfo -> caseInfo.Name, FSharpValue.MakeUnion(caseInfo, [||]))
   |> Map.ofArray
+
+let tokenToValue = function
+  | Token.RegA -> Some AbstractSyntaxTree.RegA | Token.RegB -> Some AbstractSyntaxTree.RegB | Token.RegC -> Some AbstractSyntaxTree.RegC
+  | Token.RegX -> Some AbstractSyntaxTree.RegX | Token.RegY -> Some AbstractSyntaxTree.RegX | Token.RegZ -> Some AbstractSyntaxTree.RegZ
+  | Token.RegI -> Some AbstractSyntaxTree.RegI | Token.RegJ -> Some AbstractSyntaxTree.RegJ | Token.SP -> Some AbstractSyntaxTree.SP
+  | Token.PC -> Some AbstractSyntaxTree.PC | Token.EX -> Some AbstractSyntaxTree.EX
+  | _ -> None
+
+let tokenToBasicOpcode = function
+  | Token.SET -> Some AbstractSyntaxTree.SET
+  | Token.ADD -> Some AbstractSyntaxTree.ADD | Token.SUB -> Some AbstractSyntaxTree.SUB
+  | Token.MUL -> Some AbstractSyntaxTree.MUL | Token.MLI -> Some AbstractSyntaxTree.MLI | Token.DIV -> Some AbstractSyntaxTree.DIV | Token.DVI -> Some AbstractSyntaxTree.DVI
+  | Token.AND -> Some AbstractSyntaxTree.AND | Token.BOR -> Some AbstractSyntaxTree.BOR | Token.XOR -> Some AbstractSyntaxTree.XOR
+  | Token.SHR -> Some AbstractSyntaxTree.SHR | Token.ASR -> Some AbstractSyntaxTree.ASR | Token.SHL -> Some AbstractSyntaxTree.SHL
+  | Token.IFB -> Some AbstractSyntaxTree.IFB | Token.IFC -> Some AbstractSyntaxTree.IFC | Token.IFE -> Some AbstractSyntaxTree.IFE | Token.IFN -> Some AbstractSyntaxTree.IFE
+  | Token.IFG -> Some AbstractSyntaxTree.IFG | Token.IFA -> Some AbstractSyntaxTree.IFA | Token.IFL -> Some AbstractSyntaxTree.IFL | Token.IFU -> Some AbstractSyntaxTree.IFU
+  | Token.ADX -> Some AbstractSyntaxTree.ADX | Token.SBX -> Some AbstractSyntaxTree.SBX | Token.STI -> Some AbstractSyntaxTree.STI | Token.STD -> Some AbstractSyntaxTree.STD
+  | _ -> None
+
+let tokenToSpecialOpcode = function
+  | Token.JSR -> Some AbstractSyntaxTree.JSR
+  | Token.INT -> Some AbstractSyntaxTree.INT | Token.IAG -> Some AbstractSyntaxTree.IAG | Token.IAS -> Some AbstractSyntaxTree.IAS | Token.IAQ -> Some AbstractSyntaxTree.IAQ
+  | Token.HWN -> Some AbstractSyntaxTree.HWN | Token.HWQ -> Some AbstractSyntaxTree.HWQ | Token.HWI -> Some AbstractSyntaxTree.HWI
+  | _ -> None
 
 // Converts a token to a case of a discriminated union using a map; returns none if not found
 let tokenToCase (map: Map<_, _>) tok : 'a option =
@@ -23,6 +46,7 @@ let tokenToCase (map: Map<_, _>) tok : 'a option =
 
 // Parses a single operand
 let parseOperand tokens =
+  let t, r = List.head tokens, List.tail tokens
   ()
   (*
   let (tok: Value option), r = unbox tokenToCase nameRegisterMap (List.head tokens), List.tail
@@ -42,10 +66,10 @@ let parseStatement tokens =
   let tok, r = tokenToCase nameBasicOpcodeMap (List.head tokens), List.tail
   match tok with
     | Some tok ->
-      BasicInstruction(unbox tok, Literal 42s, Literal 43s)
+      AbstractSyntaxTree.BasicInstruction(unbox tok, AbstractSyntaxTree.Literal 42s, AbstractSyntaxTree.Literal 43s)
     | None ->
-      let (tok: SpecialOpcode option), r = unbox tokenToCase nameSpecialOpcodeMap (List.head tokens), List.tail
+      let (tok: AbstractSyntaxTree.SpecialOpcode option), r = unbox tokenToCase nameSpecialOpcodeMap (List.head tokens), List.tail
       match tok with
         | Some tok ->
-          SpecialInstruction(unbox tok, Literal 42s)
+          AbstractSyntaxTree.SpecialInstruction(unbox tok, AbstractSyntaxTree.Literal 42s)
         | None -> failwith "Expected opcode"
